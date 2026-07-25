9623e08d78a2941502b6eb5a843dcb42f538d90f
quoc <vancongquoc121@gmail.com>
2026-07-25 15:29:55 +0700

    Add file bat open google colab

---
+++ open_colab.bat
@@ -0,0 +1,3 @@
+@echo off
+rem Opens the AIGenerateTool Colab notebook in the default browser.
+start "" "https://colab.research.google.com/github/vancongquoc121/AIGenerateTool/blob/main/docs/AIGenerateTool.ipynb"
